fe98bdbdfdd59d6dbdb9b590865f23c1ccb2578b
HelloChen <2421985222@qq.com>
2020-02-20 18:05:22 +0800

    1

--- C language/test/test.c
+++ C language/test/test.c
@@ -1,10 +1,20 @@
-/* Hello Chen C program */
+/* Programming Exercise 5-1 */
 #include <stdio.h>
-#include <stdlib.h>
-
+#include "stdlib.h"
 int main(void)
 {
-    float Mb=18.12,MB=2.20;
-    printf("Hello Chen %.2f %.2f %.2f\n",MB,Mb,(MB*8)/Mb);
+    const int minperhour = 60;
+    int minutes, hours, mins;
+    printf("Enter the number of minutes to convert: ");
+    scanf("%d", &minutes);
+    while (minutes > 0)
+    {
+        hours = minutes / minperhour;
+        mins = minutes % minperhour;
+        printf("%d minutes = %d hours, %d minutes\n", minutes, hours, mins);
+        printf("Enter next minutes value (0 to quit): ");
+        scanf("%d", &minutes);
+    }
+    printf("Bye\n");
     system("pause");
 }
